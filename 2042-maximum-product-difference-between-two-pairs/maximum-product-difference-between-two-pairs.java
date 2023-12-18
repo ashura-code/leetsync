0b4ec9cc1ca8a3b3a2042746241c70113a65a837
Adhitya <83760410+ashura-code@users.noreply.github.com>
2023-12-18 20:30:37 +0530

    Time: 1 ms (98.22%) | Memory: 44.5 MB (28.04%) - LeetSync

---
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int maxProductDifference(int[] nums) {
+
+        int largest = 0;
+        int largest_2 = 0;
+
+        int smallest = 10000;
+        int smallest_2 = 10000;
+
+        for(int num:nums){
+           if(num > largest_2){ 
+                if(num >= largest){ 
+                     largest_2 = largest;
+                     largest = num;
+                }
+                else{
+                    largest_2 =num;
+                }
+                
+           }
+
+           if(num < smallest_2){ 
+                if(num <= smallest){ 
+                     smallest_2 = smallest;
+                     smallest = num;
+                }
+                else{
+                    smallest_2 =num;
+                }
+                
+           }
+
+        //    if(num < smallest) {
+        //         smallest_2 = smallest;
+        //         smallest = num;
+        //    }
+
+
+            
+        }
+
+        return (largest*largest_2)-(smallest*smallest_2);
+    }
+}
