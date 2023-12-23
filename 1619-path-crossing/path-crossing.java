9d43d2503eb0c2518608e7577dd376cc3d3bb7b9
Adhitya <83760410+ashura-code@users.noreply.github.com>
2023-12-23 14:17:00 +0530

    Time: 1 ms (96.48%) | Memory: 42.1 MB (6.17%) - LeetSync

--- 1619-path-crossing/path-crossing.java
+++ 1619-path-crossing/path-crossing.java
@@ -1,39 +1,56 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Pair {
+    int x, y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pair pair = (Pair) o;
+        return x == pair.x && y == pair.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
+
 class Solution {
     public boolean isPathCrossing(String path) {
-
-        Set<String> visited = new HashSet<>();
-        visited.add("0,0");
+        Set<Pair> visited = new HashSet<>();
+        visited.add(new Pair(0, 0));
 
         int x = 0;
         int y = 0;
-           
-          for(char dir:path.toCharArray()){ 
-               if(dir == 'N'){ 
-                    y++;
-               }
-               else if(dir == 'E'){ 
-                   x++;
-               }
-               else if(dir =='W'){ 
-                   x--;
-               }
-               else if(dir =='S'){ 
-                   y--;
-               }
 
-               String current_pos =  x + "," + y;
+        for (char dir : path.toCharArray()) {
+            if (dir == 'N') {
+                y++;
+            } else if (dir == 'E') {
+                x++;
+            } else if (dir == 'W') {
+                x--;
+            } else if (dir == 'S') {
+                y--;
+            }
 
-               if (visited.contains(current_pos)) {
-                    return true;
-               }
-               else{
-                   visited.add(current_pos);
-               }
+            Pair currentPos = new Pair(x, y);
 
-               }
+            if (visited.contains(currentPos)) {
+                return true;
+            } else {
+                visited.add(currentPos);
+            }
+        }
 
-        
         return false;
-        
     }
 }
