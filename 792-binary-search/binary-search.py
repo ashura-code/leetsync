412b7c4416337263679965cbbb8e2c374c29d0f3
Adhitya <83760410+ashura-code@users.noreply.github.com>
2024-02-14 16:44:18 +0530

    Time: 40 ms (99.94%) | Memory: 17.4 MB (99.96%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,18 +1,15 @@
-import math
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums) - 1
-
-        while l <= r:
-            mid:int = math.floor((l+r)/2)
-
-            if target > nums[mid]:
-                l = mid+1
-            elif target < nums[mid]:
-                r = mid-1
-            else:
-                return mid
-        
-        return -1
-
+f = open("user.out", 'w')
+for n, t in zip(stdin, stdin):
+    n, t = n[1:-2].split(','), int(t.rstrip())
+    r, l, z = len(n)-1, 0, -1
+    while l <= r:
+        m = ((r-l)//2)+l
+        if int(n[m])>t:
+            r=m-1
+        elif int(n[m])<t:
+            l=m+1
+        else:
+            z = m
+            break
+    print(z, file=f)
+exit(0) 
