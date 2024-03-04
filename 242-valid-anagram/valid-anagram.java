f3f3b85bf7b7bee37ba717e653bd4bbdbb74efe2
Adhitya <83760410+ashura-code@users.noreply.github.com>
2024-03-04 10:52:35 +0530

    Time: 2 ms (98.07%) | Memory: 42.6 MB (94.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,30 +1,36 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length() != t.length()) return false;
+//         int[] map = new int[26];
+//         for(char c : s.toCharArray())
+//         {
+//             map[c-97]++;
+//         }
+//         for(char c : t.toCharArray())
+//         {
+//             if(map[c-97] > 0) map[c-97]--;
+//             else return false;
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false; // Anagrams must have the same length
+
+        if(s.length() != t.length())return false;
+
+        int[] map = new int[26];
+
+        for(char c:s.toCharArray()) map[c-'a']++;
+        for(char c:t.toCharArray()){ 
+              if( map[c-97]>0){
+                  map[c-97]--;
+
+              } else{ return false;}
         }
 
-        int[] counts = new int[26]; // Assuming input consists of lowercase English letters
-
-        // Count occurrences in string s
-        for (char c : s.toCharArray()) {
-            counts[c - 'a']++;
-        }
-
-        // Add occurrences in string t and check for even occurrences
-        for (char c : t.toCharArray()) {
-            if (--counts[c - 'a'] < 0) {
-                return false; // More occurrences of c in t than in s
-            }
-        }
-
-        // Check if all counts are even
-        for (int count : counts) {
-            if (count % 2 != 0) {
-                return false; // Odd occurrence found
-            }
-        }
-
-        return true; // All characters in s and t match
+        return true;
+      
     }
 }
