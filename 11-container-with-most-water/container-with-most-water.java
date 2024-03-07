519ebbc27bae112d87fb1e09b85519d41bd5a1a7
Adhitya <83760410+ashura-code@users.noreply.github.com>
2024-03-07 09:42:27 +0530

    Time: 5 ms (71.49%) | Memory: 56.9 MB (94.91%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,63 +1,27 @@
 class Solution {
     public int maxArea(int[] height) {
 
-        // int left_i = 0;
-        // int right_i = height.length - 1;
-        // int max_water =0;
+        int left_i = 0;
+        int right_i = height.length-1;
+        int max_water = Integer.MIN_VALUE;
 
-        // while(left_i < right_i){ 
-        //     int left = height[left_i];
-        //     int right = height[right_i];
-        //     int area = 0;
-
-        //     if(left < right){ 
-        //          area = left*(right_i - left_i);
-        //          left_i++;
-        //     }
-        //     else if(left > right){ 
-        //          area = right*(right_i - left_i);
-        //          right_i--;
-        //     }
-        //     else{ 
-        //         area = left*(right_i - left_i);
-        //         left_i++;
-        //         right_i--;
-        //     }
-           
-        //      if (area > max_water){
-        //              max_water = area;
-        //      }
-
-
-        // }
-
-        // return max_water;
-
-
-        int max_water = 0;
-        int left = 0;
-        int right = height.length - 1;
-
-        while(left < right){
-            int curr_height = Math.min(height[left],height[right]);
-            int curr_width = right-left;
-            int curr_water = curr_height * curr_width;
-             max_water = Math.max(max_water, curr_water);
-
-
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
+        while(left_i < right_i){
+            int distance = right_i - left_i;
+            int tall = Math.min(height[left_i],height[right_i]);
+            int area = distance*tall;
+            max_water = Math.max(max_water,area);
+            if(height[left_i]>=height[right_i]) {
+                 right_i--;
+            }else{
+                left_i++;
             }
-            
-
         }
 
-        return max_water;
+        return  max_water;
 
 
 
 
+        
     }
 }
