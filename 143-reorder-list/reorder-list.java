f63f225edc0994657ec92a17a64195ebd29756cf
Adhitya <83760410+ashura-code@users.noreply.github.com>
2024-03-10 22:58:20 +0530

    Time: 1 ms (99.96%) | Memory: 47.7 MB (88.42%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -8,77 +8,43 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
 class Solution {
-
-    public ListNode reverseList(ListNode head){ 
-            ListNode current = head;
-            ListNode previous = null;
-            ListNode nxt = null;
-
-            while(current!=null) {
-                 nxt = current.next;
-                 current.next = previous;
-                 previous = current;
-                 current = nxt;
-            }
-
-            return previous;
-         
-    }
-
-    public void MergeLists(ListNode first,ListNode second){
-
-        while(second != null) {
-             ListNode temp1 = first.next;
-             ListNode temp2 = second.next;
-
-             first.next = second;
-             second.next = temp1;
-
-             first = temp1;
-             second = temp2;
-
-        }
-
-    }
- 
-
-
     public void reorderList(ListNode head) {
 
-        if(head.next != null){ 
+         ListNode slow = head;
+         ListNode fast = head;
 
-            ListNode slow = head;
-            ListNode fast = head;
+         while(fast.next!=null && fast.next.next!=null){ 
+              slow = slow.next;
+              fast = fast.next.next;
+         }
 
-            while(fast.next !=null && fast.next.next != null){ 
-                slow = slow.next;
-                fast = fast.next.next;
-            }
+         ListNode second = slow.next;
+         ListNode first = head;
+         slow.next = null;
+         ListNode prev = null;
 
-            ListNode second_h = slow.next;
-            slow.next = null; //cut the linked list
+         while(second!=null){ 
+              ListNode temp = second.next;
+              second.next = prev;
+              prev = second;
+              second = temp;                   
+         }
 
-            //reverse the second half
-            second_h = reverseList(second_h);
-            MergeLists(head,second_h);
+         second = prev;
 
+         while(second!=null){  //deliberately second
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
 
-            
+            first.next = second;
+            second.next = temp1;
 
-            
+            first = temp1;
+            second = temp2;
 
-
-
-
-
-
-
-
-        }
-           
-        
-
-        
+         }
     }
 }
