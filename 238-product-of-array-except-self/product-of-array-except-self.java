785b093e2e0ace3ebbc7a22e17db25d54867da0c
Adhitya <83760410+ashura-code@users.noreply.github.com>
2024-07-05 23:14:28 +0530

    Time: 2 ms (86.40%) | Memory: 56.1 MB (24.01%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,27 +1,37 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) { 
+    public int[] productExceptSelf(int[] nums) {
 
-         int[] ans = new int[nums.length];
-         int[] pre = new int[nums.length];
-        int[] suff = new int[nums.length];
+        if(nums.length < 2)return nums;
 
-        pre[0] = 1;
-        suff[nums.length-1] = 1;
+        int[] left_right = new int[nums.length];
+        int[] right_left = new int[nums.length];
+        int[] ans = new int[nums.length];
 
-        for(int i = 1;i<nums.length;i++){ 
-             pre[i] = pre[i-1] * nums[i-1];
+        int multiplier = 1;
+        for(int i=0;i<nums.length;i++){ 
+             left_right[i] = nums[i]*multiplier;
+             multiplier = left_right[i];
+        }
+        multiplier = 1; //reset to 1
+        for(int i=nums.length-1;i>=0;i--){
+
+             right_left[i] = nums[i]*multiplier;
+             multiplier = right_left[i];
         }
 
-        for(int i=nums.length-2;i>=0;i--)
-        {  
-              suff[i] = suff[i+1] * nums[i+1];
-        }
+        //prefix and suffix is ready
+        ans[0] = 1 * right_left[1];
+        ans[nums.length-1] = left_right[nums.length-2];
 
-        for(int i=0;i<nums.length;i++) 
-        {
-              ans[i] = suff[i] * pre[i];
+        if(nums.length>2){ 
+            for(int i=1;i<nums.length-1;i++){ 
+                 
+                   ans[i] = left_right[i-1]*right_left[i+1];
+            
+             }
         }
 
         return ans;
+
     }
-}
+}  
