2b007cfc38c0a6b62ef30e85bde60d2baa9adfae
Adhitya <83760410+ashura-code@users.noreply.github.com>
2024-07-18 18:44:32 +0530

    Time: 25 ms (95.97%) | Memory: 51.4 MB (56.17%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,42 +1,41 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> threeSum(int[] nums) { 
 
-        List<List<Integer>> ans = new ArrayList<>();
+         
+        List<List<Integer>> result = new ArrayList<>();
 
         Arrays.sort(nums);
+        
 
-        for(int i=0;i<nums.length-2;i++) {
+        for(int i = 0;i<nums.length-2;i++){ 
 
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-
-            int l = i+1;
-            int r = nums.length-1;
+            if(i > 0 && nums[i] == nums[i-1])continue;
+            int left_i= i+1;
+            int right_i = nums.length - 1;
             int need = -1*nums[i];
 
-            while(l<r){ 
-                 if(nums[l]+nums[r] > need) {
-                      r--;
-                 }else if(nums[l]+nums[r] < need){
-                     l++;
-                 }
-                 else{
-                      List<Integer> temp  = new ArrayList<>();
-                      temp.add(nums[i]);
-                      temp.add(nums[l]);
-                      temp.add(nums[r]);
-                      ans.add(temp);
-                      l++;
-                      r--;
-
-                      while(l<r && nums[l] == temp.get(1))l++;
-                      while(l<r && nums[r] == temp.get(2))r--;    
-                 }
+            while(left_i < right_i ){ 
+                int sum = nums[left_i] + nums[right_i];
+                if(sum < need ){ 
+                     left_i++;
+                }
+                else if(sum >need){ 
+                     right_i--;
+                }
+                else{ 
+                    result.add(Arrays.asList(nums[i], nums[left_i], nums[right_i]));
+                    left_i++;
+                    right_i--;
+                    while (left_i < right_i && nums[left_i] == nums[left_i - 1]) left_i++;
+                    while (left_i < right_i && nums[right_i] == nums[right_i + 1]) right_i--;
+                }
             }
 
         }
 
-              return ans; 
+
+        return result;
+        
+
     }
 }
